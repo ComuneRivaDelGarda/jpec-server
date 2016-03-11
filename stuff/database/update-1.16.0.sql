629f6c5c08edd30febf5a714ccdd37c5994476a9
Mirco <amirco@gmail.com>
2016-03-11 17:19:54 +0100

    aggiornato script aggiornamento regole

--- stuff/database/update-1.16.0.sql
+++ stuff/database/update-1.16.0.sql
@@ -1,7 +1,14 @@
-update jpec.pec06_regole set pec06_evento = 'IMPORTA_MESSAGGIO' where pec06_evento = 'IMPORTA';
-update jpec.pec06_regole set pec06_evento = 'PROTOCOLLA_MESSAGGIO' where pec06_evento = 'PROTOCOLLA';
 
-delete from jpec.pec03_config where pec03_nome = 'PEC_PROTOCOLLO_IMPL';
+select * from generale.costante where nome = 'PEC_PROTOCOLLO_IMPL';
+delete from generale.costante where nome = 'PEC_PROTOCOLLO_IMPL';
 
 -- alter table jpec.pec06_regole drop column pec06_classe;
-alter table jpec.pec06_regole add column pec06_classe longtext;
+ALTER TABLE pec.regole ADD COLUMN classe varchar;
+
+update pec.regole set evento = 'IMPORTA_MESSAGGIO' where evento = 'IMPORTA';
+update pec.regole set evento = 'PROTOCOLLA_MESSAGGIO' where evento = 'PROTOCOLLA';
+
+update pec.regole set classe = 'it.attocchi.jpec.server.protocollo.impl.ProtocolloGDAClient' where evento = 'PROTOCOLLA_MESSAGGIO'; 
+
+-- abilita store EML per poter avere EML Ricevuta da Archiviare in cambio stato
+update generale.costante set valore = 'true' where nome = 'PEC_ENABLE_EML_STORE';
