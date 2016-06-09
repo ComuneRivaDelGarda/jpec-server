fdae1ad767c58219900a5344b935e1e0dc7f3ba6
Mirco <amirco@gmail.com>
2016-06-09 15:00:09 +0200

    adeguamento script per postgres

--- stuff/database/update-1.21.0.sql
+++ stuff/database/update-1.21.0.sql
@@ -1,7 +1,7 @@
 
-ALTER TABLE jpec.pec01_messaggi ADD COLUMN pec01_segnatura_xml longtext;
+ALTER TABLE pec.messaggi ADD COLUMN segnatura_xml varchar;
 
 -- cambia il filtro delle ricevute soloRicevuteConRiferimento quindi cercare quante sono:
 -- xRicevuta  ma senza xRiferimentoMessageID
-select pec01_x_ricevuta, pec01_x_riferimento_message_id from pec01_messaggi;
-select * from pec01_messaggi WHERE pec01_x_ricevuta <> '' and (pec01_x_riferimento_message_id is null or pec01_x_riferimento_message_id = '');
+select x_ricevuta, x_riferimento_message_id from pec.messaggi;
+select * from pec.messaggi WHERE x_ricevuta <> '' and (x_riferimento_message_id is null or x_riferimento_message_id = '');
