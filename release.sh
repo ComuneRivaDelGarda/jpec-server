c849067e21c3069764fd528dec5a8ec91704f818
Mirco <amirco@gmail.com>
2016-11-18 09:55:35 +0100

    script release

---
+++ release.sh
@@ -0,0 +1,9 @@
+cd /home/mirco/Progetti/ivSource/gda/jpec-server-riva
+git checkout master
+git merge develop
+#git checkout develop
+
+cd /home/mirco/Progetti/ivSource/gda/gdapec-protocollo-client
+git checkout master
+git merge develop
+#git checkout develop
